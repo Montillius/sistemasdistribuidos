950ebac8c87afab06beaaf226ab6cb8b0687e5b5
OtilianoJunior <otiliano.rodrigues@veri.com.br>
2022-11-30 16:22:12 -0300

    refactor: atualização do código com comentários

--- client/socket_client.py
+++ client/socket_client.py
@@ -1,24 +1,52 @@
 import socket
+from datetime import datetime
 
 
 def client_program():
-    host = socket.gethostname()  # as both code is running on same pc
-    PORTA = 5000  # socket server port number
+    host = socket.gethostname()
+    # o host irá receber o conexão com o servidor para que o encontre
 
-    client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, PORTA))  # connect to the server
+    PORTA = 5000
+    # Usando a mesma analogia do server, a porta é por onde de fato os pacotes "entram" no servidor ou seja
+    # é necessário que seja a mesma para que ocorra a comunicação
 
-    message = input(" -> ")  # take input
+    client_socket = socket.socket()
+    # Instância do objeto do tipo socket no formatto TCP/IP
 
-    while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
+    client_socket.connect((host, PORTA))
+    # a fução connect estabele a conexão com o servidor
+    # Quando a connect (conexão) foi estabelecida, o soquete pode ser utilizado para enviar uma solicitação de
+    # texto para o servidor. O mesmo soquete é que irá ler a resposta.
 
-        print('Received from server: ' + data)  # show in terminal
+    data_sistema_americano = datetime.now()
+    data_sistema_format = data_sistema_americano.strftime('%d/%m/%Y %H:%M:%S')
+    # Configuração da hora do envio e recebimento das mensagens
 
-        message = input(" -> ")  # again take input
+    message = input(' -> ') + (' Hora da mensagem: ' + data_sistema_format)
+    # Entrada da mensagem
 
-    client_socket.close()  # close the connection
+    while message.lower().strip() != 'finalizar serviço':
+        # loop criado para deixa o programa em execução se o client enviar a mensagem 'finalizar serviço'
+        # o loop encerrará. O metofo lower transforma todos os caracteres em minusculos e o strip procura dentro
+        # da mensagem o texto que foi especificado
+
+        client_socket.send(message.encode())
+        # envia a mensagem do cliente para o servidor
+
+        data = client_socket.recv(1024).decode()
+        #  Aguarda um dado respondido pelo servidor de até 1024 Bytes, a função ‘recv’ possui somente 1 argumento
+        # que é o tamanho do Buffer.
+
+        print('Resposta do servidor: ' + data)
+        print('Hora atual do servidor ' + data_sistema_format)
+        # Irá printar  a mensagem recebida do servidor e a hora atual do sistema
+
+        message = input(' -> ') + (' Hora da mensagem: ' + data_sistema_format)
+        # exibe novamente a entrada de dados para que o cliente se comunique, enquanto a condição for verdadeira
+        # irá ser executado
+
+    client_socket.close()
+    # finaliza a conexão com o servidor
 
 
 if __name__ == '__main__':
--- server/socket_server.py
+++ server/socket_server.py
@@ -1,10 +1,12 @@
 import socket
+from datetime import datetime
 
 
 def server_program():
     host = '10.0.0.203'
     # um nome de host é um rótulo atribuído a um dispositivo conectado a uma rede de computadores é usado
     # para identificar o dispositivo
+
     SERVER_PORT = 5000
     # iniciando a porta acima da 1024
 
@@ -16,7 +18,6 @@ def server_program():
     #  O comando “bind” vincula o endereço como exemplo 10.1.1.1 do servidor e a porta 5000
     #  ao socket que foi criado pelo sistema operacional do servidor.
 
-
     server_socket.listen(1)
     # O nosso servidor TCP/IP vai receber 1 conexão por vez e responder ao cliente uma confirmação do recebimento
     # da mensagem. A função listen() coloca o socket no modo servidor, e define a quantidade de solicitações
@@ -27,16 +28,36 @@ def server_program():
     # O novo socket utiliza uma nova porta atribuída automaticamente pelo kernel.
 
     print("Conexão de: " + str(address))
-    while True:
-        data = conn.recv(1024).decode()
-        if not data:
-            # if data is not received break
-            break
-        print("from connected user: " + str(data))
-        data = input(' -> ')
-        conn.send(data.encode())  # send data to the client
+    # Ao estabelecer uma conexão com o clinte irá printar está conexão pro servidor
 
-    conn.close()  # close the connection
+    while True:
+        # condição que deve ser verdadeira, para continuar  a executar o servidor
+
+        data = conn.recv(1024).decode()
+        # Aguarda um dado enviado pela rede de até 1024 Bytes, a função ‘recv’ possui somente 1 argumento
+        # que é o tamanho do Buffer.
+
+        # Este método é usado para converter de um esquema de codificação, no qual a string de argumento é
+        # codificada para o esquema de codificação desejado.
+
+        data_sistema_americano = datetime.now()
+        data_sistema_format = data_sistema_americano.strftime('%d/%m/%Y %H:%M:%S')
+        # Configuração da hora do envio e recebimento das mensagens
+
+        if not data:
+            # O data irá receber a mensagem do cliente, se os dados não forem recebidos irá executar o break
+            break
+        print("O usuário conectado enviou: " + str(data))
+        print('Hora atual do servidor ' + data_sistema_format)
+        # Irá printar  a mensagem enviada pelo cliente e a hora atual do sistema
+
+        data = input(' -> ') + (' Hora da mensagem: ' + data_sistema_format)
+        # data que recebe a mensagem do servidor para enviar pro cliente, juntamente do horário
+        conn.send(data.encode())
+        # envia a mensagem do servidor para o cliente
+
+    conn.close()
+    # finaliza a conexão
 
 
 if __name__ == '__main__':
