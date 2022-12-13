e6c2a96e3ff339528d413c98e6c8943109745b70
Montillius <otiliano.jr@gmail.com>
2022-12-13 19:05:46 -0300

    attt

---
+++ teste_2/cliente.py
@@ -0,0 +1,110 @@
+from socket import *
+import sys
+import time
+from threading import Thread
+
+PORT = 2343
+
+
+class cliente(Thread):
+    # subclasse da classe Thread. essa classe conecta com o servidor e envia os dados para serem calculados
+    def __init__(self, ip, trials):
+        # construtor da classe
+
+        Thread.__init__(self)
+        # chama o construtor da classe pai
+
+        self.ip = ip
+        # o ip que deve conectar
+
+        self.hits = -1
+        # armazena o resultado
+
+        self.trials = str(trials)
+        # numero de tentativas
+
+    def run(self):
+        # essa eh a parte que serah executada
+
+        print("thread do ip " + str(self.ip) + " iniciou")
+        # abre a conexao com o servidor
+
+        s = socket(AF_INET, SOCK_STREAM)
+        s.connect((self.ip, PORT))
+        # envia para o servidor o numero de tentativas
+
+        s.send(self.trials)
+        # fica esperando o resultado do calculo do servidor
+
+        self.hits = s.recv(1024)
+        s.close()
+        print("trhread do ip " + str(self.ip) + " finalizou com o resultado=" + str(self.hits))
+
+
+def ativos(servidores):
+    # funcao que verifica se os servidores estao ativos
+
+    ativos = []
+    # lista com os servidores ativos
+
+    for ip in servidores:
+        try:
+            print("testando " + str(ip))
+            s = socket(AF_INET, SOCK_STREAM)
+            s.connect((ip, PORT))
+            s.send("PING")
+            # manda um comando ping para ver se o servidor ainda esta ativo
+            s.close()
+            ativos.append(ip)
+        except:
+            # print "Unexpected error:", sys.exc_info()[0]
+            pass
+        return ativos
+
+
+trials = sys.argv
+# recebe do parametro do usuario o numero maximo de tentativas
+
+print("Verificando servidores ativos")
+# verifica o numero de servidores ativos
+
+servidores = ativos(['192.168.200.14', '200.135.240.1', '192.168.200.11', '192.168.200.5'])
+
+if len(servidores) == 0:
+    print("Nao existem servidores ativos")
+    sys.exit()
+
+print("Servidores ativos:" + str(servidores))
+# calcula o numero de tentativas que cada servidor deve executar
+
+trials_por_servidor = int(trials) / len(servidores)
+
+resultado = []
+# lista com as threads em execucao
+
+t0 = time.time()
+# usado para o calculo do tempo de execucao
+
+for ip in servidores:
+    atual = cliente(ip, trials_por_servidor)
+    # cria uma nova thread
+
+    resultado.append(atual)
+    # adiciona a thread na lista de threads
+
+    atual.start()
+    # inicia a thread
+
+total_hits = 0
+# total dos resultados
+
+for r in resultado:
+    r.join()
+    # espera ate a thread terminar
+    total_hits += int(r.hits)
+
+pi = 4.0 * int(total_hits) / int(trials)
+# faz o calculo do pi usando os resultados enviados por cada servidor
+print("Pi :" + str(pi))
+tf = time.time()
+print('O tempo gasto na execucao eh: ', tf - t0, '[s]')
---
+++ teste_2/servidor.py
@@ -0,0 +1,54 @@
+import random  
+# gerador de numeros aleatorios
+
+from socket import *  
+# sockets
+
+
+# funcao que faz o calculo do numero de pontos
+def pi(trials):
+    hits = 0
+    for i in range(trials):
+        x = random.random()
+        y = random.random()
+        if (((x * x) + (y * y)) < 1):
+            hits = hits + 1
+    return hits
+
+
+HOST = ''
+PORT = 2343
+
+s = socket(AF_INET, SOCK_STREAM)  
+# cria um socket
+
+s.bind((HOST, PORT))  
+# conecta o socket na porta
+
+s.listen(1)  
+# fica esperando conexoes
+
+print('Escutando conexoes')
+while 1:
+    conn, addr = s.accept() 
+    # recebe uma conexao
+    
+    data = conn.recv(1024)  
+    # recebe o dado enviado pelo cliente
+    
+    if data == "PING":  
+        # se o comando recebido foi um ping
+        print("ping recebido")
+        
+    else:  
+        # senao a informacao recebida eh o numero de tentativas
+        
+        print("Calculando para o cliente " + str(addr[0]))
+        hits = pi(int(data))  
+        # calcula o numero de pontos usando o numero de tentativas recebidas do cliente
+        print(hits)
+        conn.send(str(hits))  
+        # retorna ao cliente o numero de pontos
+        
+        conn.close()  
+        # fecha a conexao
